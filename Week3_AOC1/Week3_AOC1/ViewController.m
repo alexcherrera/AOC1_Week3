17ea10b923379933d50c9d566dce4080b90401a9
Alexander Herrera <alexcherrera@fullsail.edu>
2012-11-08 16:48:33 -0500

    Finished to submit

--- Week3_AOC1/Week3_AOC1/ViewController.m
+++ Week3_AOC1/Week3_AOC1/ViewController.m
@@ -20,42 +20,34 @@
 	carOne = 25000;
 	carTwo = 40000;
 	maxValue = 70000;
-	twoIntValues = [NSArray arrayWithObjects:carOne, carTwo, nil];
-	//twoIntValues = []
 //Calling, passing, and capturing the values to get the total sum of both:
 	totalAdd = [self addInt1:carOne addingInt2:carTwo];
-	NSLog(@"total is %i", totalAdd);
+	NSLog(@"total is %d", totalAdd);
+//NSNumber and converting to NSString:
+	bundleInt = [[NSNumber alloc]initWithInt:(int)totalAdd];
+	convertToString = [bundleInt stringValue];
+//Initializing, calling, and passing of the appending strings:
+	firstString = [[NSString alloc]initWithString:@"The final value"];
+	secondString = [[NSString alloc]initWithFormat:@" would be $%@", convertToString];
+	finalString = [[NSString alloc]init];
+	finalString = [self appendStrings:firstString appendingToString:secondString];
+//Captured the result and passing to be displayed (UIAlertView):
+	[self displayAlertWithString:finalString];
+	NSLog(@"%@",finalString);
 //To get YES or NO, Call the function and pass the values:
 	compareResults = [self boolCompare:totalAdd boolComparing2:maxValue];
 	//To display the results using an if statement:
-		if (compareResults == YES)
+		twoValueBool = [[NSString alloc]initWithFormat:@"Value 1 is %d, value 2 is %d, and the result for both is %d",(int)carOne, (int)carTwo, (int)totalAdd];
+		if (compareResults)
 		{
-			[self displayAlertWithString:(NSString*)carOne];
-			NSLog(@"Yes we can buy both cars together");
+			[self displayAlertWithString:twoValueBool];
+			NSLog(@"Yes");
 		} else
 		  {
-			NSLog(@"We can only buy one car");
+			NSLog(@"No");
 		  }
-//Initializing, calling, and passing of the appending strings:
-	firstString = [[NSString alloc]initWithString:@"We want to buy"];
-	secondString = [[NSString alloc]initWithString:@" both of the cars"];
-	finalString = [self appendStrings:firstString appendingToString:secondString];
-	formatString = [[NSString alloc]initWithFormat:@"%@ and if the price is $%i we will buy it today!",finalString, totalAdd ];
-	NSLog(@"%@",finalString);
-	NSLog(@"%@", formatString);
-
-//Displaying with an alert:
-	//UIAlertView * stringAlert = [[UIAlertView alloc] initWithTitle:
-	//Captured the result and passing to be displayed (UIAlertView):
-	[self displayAlertWithString:formatString];
-	
-//NSNumber and converting to NSString:
-	bundleInt = [[NSNumber alloc]initWithInt:totalAdd];
-	convertToString = [bundleInt stringValue];
-	[self displayAlertWithString:convertToString];
 	
 	[super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
 }
 
 - (void)didReceiveMemoryWarning
@@ -70,6 +62,14 @@
 	return total;
 	
 }
+//Appending strings to get final string:
+-(NSString*)appendStrings:(NSString*)lonelyString appendingToString:(NSString*)combindString
+{
+	mutableString = [[NSMutableString alloc]initWithString:lonelyString];
+	stringToCapture = [[NSString alloc]init];
+	stringToCapture = [mutableString stringByAppendingString:combindString];
+	return stringToCapture;
+}
 //Boolean Comparison Function:
 -(BOOL)boolCompare:(NSInteger)totalValue boolComparing2:(NSInteger)maxNumValue
 {
@@ -81,26 +81,14 @@
 		return NO;
 	  }
 }
-//Appending strings to get final string:
--(NSString*)appendStrings:(NSString*)lonelyString appendingToString:(NSString*)combindString
-{
-	mutableString = [[NSMutableString alloc]initWithString:lonelyString];
-	stringToCapture = [[NSString alloc]init];
-	stringToCapture = [mutableString stringByAppendingString:combindString];
-	//[self displayAlertWithString:stringToCapture];
-	return stringToCapture;
-}
 -(NSString*)displayAlertWithString:(NSString*)displayString
 {
-	stringAlert = [[UIAlertView alloc] initWithTitle:@"Car Result" message:displayString delegate:nil cancelButtonTitle:@"Buy Now" otherButtonTitles:nil, nil];
+	stringAlert = [[UIAlertView alloc] initWithTitle:@"Car Result" message:displayString delegate:nil cancelButtonTitle:@"ok" otherButtonTitles:nil, nil];
 	if(stringAlert !=nil)
 	{
 		[stringAlert show];
 	}
-	/*if (displayString === convertToString)
-	{
-		
-	}*/
+	
 	return 0;
 }
 @end
